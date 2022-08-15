154b8e6bf07f181f77959d12a71a00d7abbd3cf4
이경범 <tkdwk567@naver.com>
2022-08-15 18:14:10 +0900

    Item repository 생성

---
+++ src/main/java/com/example/jpa_study/project/domain/repository/ItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.jpa_study.project.domain.repository;
+
+import com.example.jpa_study.project.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
